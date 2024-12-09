a809275a3a058a5793d848390348c904865722bd
Tanmay Patel <tanmaykpatel03@gmail.com>
2024-12-09 22:20:45 +0530

    Model trainer Done.

--- main.py
+++ main.py
@@ -8,38 +8,38 @@ from src.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 
 
 
-# STAGE_NAME="Data Ingestion Stage"
+STAGE_NAME="Data Ingestion Stage"
 
-# try:
-#     logger.info(f">>>>> Stage {STAGE_NAME} Started <<<<<")
-#     obj = DataIngestionTrainingPipeline()
-#     obj.initiate_data_ingestion()
-#     logger.info(f">>>>> Stage {STAGE_NAME} Completed <<<<<\n\nx===========x")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+try:
+    logger.info(f">>>>> Stage {STAGE_NAME} Started <<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.initiate_data_ingestion()
+    logger.info(f">>>>> Stage {STAGE_NAME} Completed <<<<<\n\nx===========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
-# STAGE_NAME="Data Validation Stage"
+STAGE_NAME="Data Validation Stage"
 
-# try:
-#     logger.info(f">>>>> Stage {STAGE_NAME} Started <<<<<")
-#     obj = DataValidationTrainingPipeline()
-#     obj.initiate_data_validation()
-#     logger.info(f">>>>> Stage {STAGE_NAME} Completed <<<<<\n\nx===========x")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+try:
+    logger.info(f">>>>> Stage {STAGE_NAME} Started <<<<<")
+    obj = DataValidationTrainingPipeline()
+    obj.initiate_data_validation()
+    logger.info(f">>>>> Stage {STAGE_NAME} Completed <<<<<\n\nx===========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
-# STAGE_NAME="Data Transformation Stage"
+STAGE_NAME="Data Transformation Stage"
 
-# try:
-#     logger.info(f">>>>> Stage {STAGE_NAME} Started <<<<<")
-#     obj = DataTransformationTrainingPipeline()
-#     obj.initiate_data_transformation()
-#     logger.info(f">>>>> Stage {STAGE_NAME} Completed <<<<<\n\nx===========x")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+try:
+    logger.info(f">>>>> Stage {STAGE_NAME} Started <<<<<")
+    obj = DataTransformationTrainingPipeline()
+    obj.initiate_data_transformation()
+    logger.info(f">>>>> Stage {STAGE_NAME} Completed <<<<<\n\nx===========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 STAGE_NAME="Model Trainer stage"
 
