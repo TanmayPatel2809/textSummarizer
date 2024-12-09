e2d12e1a7b5f26d240bef5e6aed9a494bc111b67
root <tanmaykpatel03@gmail.com>
2024-12-09 19:25:29 +0000

    params updated

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,11 +1,17 @@
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+from transformers import BartForConditionalGeneration, BartTokenizer
 from transformers import TrainingArguments, Trainer
 from transformers import DataCollatorForSeq2Seq
 import torch
 from datasets import load_from_disk
 import os
 from src.entity.config_entity import ModelTrainerConfig
-from src.components.compute_metrics import compute_metrics
+
+from evaluate import load
+from transformers import pipeline
+import numpy as np
+
+# Load ROUGE metric
+rouge_metric = load("rouge")
 
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 
@@ -13,10 +19,32 @@ class ModelTrainer:
     def __init__(self, config: ModelTrainerConfig):
         self.config = config
 
+    def compute_metrics(self,eval_pred):
+        tokenizer = BartTokenizer.from_pretrained(self.config.model_ckpt)
+
+        predictions, labels = eval_pred# Obtaining predictions and true labels
+        
+        decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+        
+        labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+        decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+        decoded_preds = ["\n".join(nltk.sent_tokenize(pred.strip())) for pred in decoded_preds]
+        decoded_labels = ["\n".join(nltk.sent_tokenize(label.strip())) for label in decoded_labels]
+        
+        
+        result = metric.compute(predictions=decoded_preds, references=decoded_labels, use_stemmer=True)
+        result = {key: value.mid.fmeasure * 100 for key, value in result.items()} # Extracting some results
+
+        prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in predictions]
+        result["gen_len"] = np.mean(prediction_lens)
+
+        return {k: round(v, 4) for k, v in result.items()}
+    
     def train(self):
         device = "cuda" if torch.cuda.is_available() else "cpu"
-        tokenizer = AutoTokenizer.from_pretrained(self.config.model_ckpt)
-        model = AutoModelForSeq2SeqLM.from_pretrained(self.config.model_ckpt).to(device)
+        tokenizer = BartTokenizer.from_pretrained(self.config.model_ckpt)
+        model = BartForConditionalGeneration.from_pretrained(self.config.model_ckpt).to(device)
         data_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
 
         #loading the data
@@ -41,9 +69,9 @@ class ModelTrainer:
             eval_dataset=dataset["validation"],
             tokenizer=tokenizer,
             data_collator=data_collator,
-            compute_metrics=compute_metrics,
+            compute_metrics=self.compute_metrics,
         )
-        
+        torch.cuda.empty_cache()
         trainer.train()
 
         ## Save model
